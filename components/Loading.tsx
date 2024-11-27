4f4f822a0de1c4c38c886cf61182dfad4d251313
Ali Mohammad <113783570+AliHMohammad@users.noreply.github.com>
2024-11-27 10:12:02 +0100

    feat: now shows splash screen when loading (#16)

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,12 +1,10 @@
-import { Text, View } from "react-native";
+import { View, Image } from "react-native";
 import React from "react";
 
 export const Loading = () => {
     return (
-        <View className="flex-1 bg-red-500 justify-center items-center">
-            <Text className="text-blue-500 text-6xl">LOADING</Text>
+        <View className="flex-1">
+            <Image className="w-full h-full" source={require("../assets/splash.png")} />
         </View>
     );
 };
-
-

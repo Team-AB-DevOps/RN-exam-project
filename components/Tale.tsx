9e3ccce8c475b324c65bf471fb11d4e83c455053
Forkeh <forkeh@gmail.com>
2024-12-04 23:09:37 +0100

    feat: tale images now show on homepage, also after just being created

--- components/Tale.tsx
+++ components/Tale.tsx
@@ -1,9 +1,14 @@
-import { View, Text, ViewProps, Pressable } from "react-native";
+import { View, Text, ViewProps, Pressable, Image } from "react-native";
 import React from "react";
 import ITale from "../models/Tale";
+import { useDocument } from "react-firebase-hooks/firestore";
+import { doc, getFirestore } from "firebase/firestore";
+import { app, storage } from "../firebase";
+import { getDownloadURL, ref } from "firebase/storage";
 
 interface TaleDisplayProps extends ViewProps {
     tale: ITale;
+    userId?: string | undefined;
     onSwipeRight?: (id: string) => void; // Delete
     onPress?: (id: string) => void;
 }
@@ -22,10 +27,22 @@ export const TaleListItem = (props: TaleDisplayProps) => {
 };
 
 export const TaleGridItem = (props: TaleDisplayProps) => {
-    const { tale, onPress, ...restProps } = props;
+    const [imagePath, setImagePath] = React.useState<string>("");
+    const { tale, onPress, userId, ...restProps } = props;
+
+    React.useEffect(() => {
+        const fetchImage = async () => {
+            const storageRef = ref(storage, `${userId}/${tale.id}/`);
+            const url = await getDownloadURL(storageRef);
+            setImagePath(url);
+        };
+
+        fetchImage();
+    }, [tale.id, userId]);
 
     return (
         <Pressable {...restProps} className="border rounded-md p-2 w-[47%]" onPress={() => onPress && onPress(tale.id!)}>
+            <Image className="w-full h-80" src={imagePath} />
             <Text className="font-semibold">{props.tale.title}</Text>
             <Text className="text-sm text-gray-600">{props.tale.description}</Text>
         </Pressable>

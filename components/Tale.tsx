1a6d57b46035c4dca02043e7b5b74990172dd25e
Forkeh <forkeh@gmail.com>
2024-12-05 12:04:01 +0100

    refactor: Tale list item's description only shows 50 chars

--- components/Tale.tsx
+++ components/Tale.tsx
@@ -16,11 +16,13 @@ interface TaleDisplayProps extends ViewProps {
 export const TaleListItem = (props: TaleDisplayProps) => {
     const { tale, onPress, ...restProps } = props;
 
+    const shortDescription = tale.description.length > 50 ? tale.description.slice(0, 50) + "..." : tale.description;
+
     return (
         <Pressable onPress={() => onPress && onPress(tale.id!)}>
             <View {...restProps} className="border rounded-md m-2 p-4 bg-red-300">
-                <Text className="font-semibold">{props.tale.title}</Text>
-                <Text className="text-sm text-gray-600">{props.tale.description}</Text>
+                <Text className="font-semibold">{tale.title}</Text>
+                <Text className="text-sm text-gray-600">{shortDescription}</Text>
             </View>
         </Pressable>
     );
